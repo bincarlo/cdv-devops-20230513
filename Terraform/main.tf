cac96413522dda640cb74a261348024d35e48344
CDV-KK <kkesy1@edu.cdv.pl>
2023-05-13 15:49:29 +0200

    create sa

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -134,3 +134,12 @@ module "bastion" {
 #   resource_group_name = module.la_resource_group.resource_group_name
 #   location            = var.location
 # }
+
+module "frontend" {
+  source              = "./modules/storage_account"
+  application         = var.application
+  environment         = var.environment
+  owner               = var.owner
+  location            = var.location
+  resource_group_name = module.resource_group.resource_group_name
+}
